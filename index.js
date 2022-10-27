a88e36e6867a3a0d14c01611f99cd137d8661286
jaspia-1 <aspia2001.icloud@gmail.com>
2022-10-27 09:41:31 +0600

    can find data  according to id

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const courses = require('./data/courses.json')
+const catagories = require('./data/catagory.json');
+app.get('/courses/:id', (req, res) => {
+    const CatagoryId = req.params.id;
+    if (CatagoryId === 'all') {
+        res.send(courses);
+    }
+    else {
+
+        const catagoryCourses = courses.filter(c => c.catagory === req.params.id);
+        res.send(catagoryCourses)
+    }
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
